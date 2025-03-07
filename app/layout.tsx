65f5db9faf78295abcb057e527aa07e32e786a0a
Iranks20 <rankunda48@gmail.com>
2025-03-07 11:02:46 +0300

    update metadata for application title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Brian Research Evidence System',
+  description: "A robust digital evidence management system tailored for Brian's research in BCFCI.",
+  generator: 'Brian Research',
 }
 
 export default function RootLayout({
